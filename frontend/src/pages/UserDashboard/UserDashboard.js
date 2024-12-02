dee4b113fea6c5ff92a6512b115e8da314e64d60
Amol Netke <amol01netke@gmail.com>
2024-12-03 00:22:18 +0530

    truffle installed and initiated!

--- frontend/src/pages/UserDashboard/UserDashboard.js
+++ frontend/src/pages/UserDashboard/UserDashboard.js
@@ -4,34 +4,71 @@ import "./UserDashboard.css";
 import React, { useState } from "react";
 import { useEffect } from "react";
 import getWeb3 from "../../handlers/Web3Handler";
+import contractABI from "../../../../contracts/";
+const contractAddress="";
 
 const GeneratorDashboard=()=>{
     const [userWalletAddress,setUserWalletAddress]=useState("");
+    const [contract,setContract]=useState(null);
+    const [mintAmount,setMintAmount]=useState(0);
     
-    useEffect(()=>{
-        const initializeWeb3=async()=>{
-            try{
-                const web3=await getWeb3();
-                console.log('Web 3 initialized!',web3);
+    const handleConnectWallet=async()=>{
+        try{
+            const web3=await getWeb3();
+            console.log('Web 3 initialized!',web3);
 
-                const accounts = await web3.eth.getAccounts();
+            const accounts = await web3.eth.getAccounts();
 
-                if (accounts.length > 0) {
-                    setUserWalletAddress(accounts[0]);
-                    console.log(`Connected Wallet Address: ${accounts[0]}`);
-                }
-            }catch(error){
-                console.error(`Falied to initialize Web 3!`,)
+            if (accounts.length > 0) {
+                setUserWalletAddress(accounts[0]);
+                console.log(`Connected Wallet Address: ${accounts[0]}`);
+
+                const contractInstance = new web3.eth.Contract(contractABI, contractAddress);
+                setContract(contractInstance);
             }
-        };
+        }catch(error){
+            console.error(`Falied to initialize Web 3!`,)
+        }
+    }
+    
+    const handleMintTokens=async()=>{
 
-        initializeWeb3();
-    },[]);
+    }
+
+    // useEffect(()=>{
+    //     const initializeWeb3=async()=>{
+    //         try{
+    //             const web3=await getWeb3();
+    //             console.log('Web 3 initialized!',web3);
+
+    //             const accounts = await web3.eth.getAccounts();
+
+    //             if (accounts.length > 0) {
+    //                 setUserWalletAddress(accounts[0]);
+    //                 console.log(`Connected Wallet Address: ${accounts[0]}`);
+    //             }
+    //         }catch(error){
+    //             console.error(`Falied to initialize Web 3!`,)
+    //         }
+    //     };
+
+    //     initializeWeb3();
+    // },[]);
     
     return (
     <React.Fragment>
         <div>
             <h1>GENERATOR DASHBOARD</h1>
+            <br></br>
+            <button onClick={handleConnectWallet}>Connect Wallet</button>
+            <h3>User Ethereum Wallet Address : {userWalletAddress}</h3>
+            <br></br>
+            <input type="number" 
+                placeholder="Enter amount to mint" 
+                value={mintAmount} 
+                onChange={(e)=>setMintAmount(e.target.value)}
+            />
+            <button onClick={handleMintTokens}Mint Tokens>Mint Tokens</button>
         </div>
     </React.Fragment>);
 }
